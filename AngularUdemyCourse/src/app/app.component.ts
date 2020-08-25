383a3c772d18681997b69f5db230b4ef1445485f
Guillaume Maiano <guillaume@maiano.fr>
2020-08-25 15:56:36 +0200

    A basic functional screen that displays a pseudo password

--- AngularUdemyCourse/src/app/app.component.ts
+++ AngularUdemyCourse/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularUdemyCourse';
+  options = { useLetters: false, useSymbols: false, useNumbers: false};
+
+  password = { value: "", desiredLength: 0};
+
+  onChangeLength(value: string) {
+    const parsedLength = parseInt(value);
+    if (!isNaN(parsedLength)) {
+      this.password.desiredLength = parsedLength;
+    }
+  }
+  onChangeUseLetters() {
+    this.options.useLetters = !this.options.useLetters;
+  }
+  onChangeUseSymbols() {
+    this.options.useSymbols = !this.options.useSymbols;
+  }
+  onChangeUseNumbers() {
+    this.options.useNumbers = !this.options.useNumbers;
+  }
+
+  onButtonClick() {
+    this.password.value = "1generatedPWD!";
+    console.log('Super secure log: ' + this.password.value + ` with options ${this.options.useLetters}`);
+  }
 }
