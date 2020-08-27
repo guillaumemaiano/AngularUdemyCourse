120bc12e0dd142ed5f18fa4cfb6a3d04c03f5c60
Guillaume Maiano <guillaume@maiano.fr>
2020-08-27 15:20:14 +0200

    a naive first implementation

--- AngularUdemyTypingChallenge/src/app/app.component.ts
+++ AngularUdemyTypingChallenge/src/app/app.component.ts
@@ -5,18 +5,52 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  sentence = 'I can\'t type that fast.';
 
-  onChangeLength(content) {
+  constructor() {
+    this.generateNewGame();
+  }
 
+  THESAURUS = [
+    "a", "body", "monkey", "part", "jumps", "wild", "shark", "the", "gun", "over", "moon", "I", "can't", "type", "that", "fast", 'won\'t'
+  ];
+
+  MAX_SIZE = 12;
+
+  sentence = '';
+
+  filledValue = "";
+
+
+  onChangeSentence(content: string) {
+    this.filledValue = content;
+    if (content.length > this.sentence.length) {
+      return;
+    }
   };
 
   onButtonClick() {
+    this.generateNewGame();
 
   };
   success(): boolean {
+    if (this.filledValue.length > this.sentence.length) {
+      return false;
+    }
+    if (this.filledValue == this.sentence) {
+      return true;
+    }
     return false;
+  };
+
+  private generateNewGame() {
+    this.sentence = '';
+    length = Math.ceil(Math.random() * this.MAX_SIZE);
+    for (var iterator = 0; iterator < length; iterator++) {
+      this.sentence += ` ${this.THESAURUS[Math.floor(Math.random()*this.THESAURUS.length)]}`;
+    }
+    this.sentence = this.sentence.trim();
   };
 
 }
