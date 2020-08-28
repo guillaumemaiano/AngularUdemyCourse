8b522bcfcceaf622decfae946936b156b0b2822d
Guillaume Maiano <guillaume@maiano.fr>
2020-08-28 21:43:55 +0200

    demonstrates ngClass

--- AngularUdemyJobs/src/app/app.component.ts
+++ AngularUdemyJobs/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularUdemyJobs';
+  currentPage= 0;
+  jobs = [
+    {
+      company: "BIG CORP SAS",
+      url:'https://images.unsplash.com/photo-1527259216948-b0c66d6fc31f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "COFFEE SHOP SLTD",
+      url:'',
+      date: 'https://images.unsplash.com/photo-1508766917616-d22f3f1eea14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
+    },
+    {
+      company: "SMALL STARTUP CO",
+      url:'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80',
+      date: ''
+    },
+    {
+      company: "BIG OIL SS",
+      url:'https://images.unsplash.com/photo-1562237553-fd52cb2067b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    }
+  ];
 }
