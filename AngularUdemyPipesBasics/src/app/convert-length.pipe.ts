f29be8c30579aabf643e42f7ac3295ceb736ed23
Guillaume Maiano <guillaume@maiano.fr>
2020-08-28 10:44:25 +0200

    Deviated from course again, but this is more useful to me...

--- AngularUdemyPipesBasics/src/app/convert-length.pipe.ts
+++ AngularUdemyPipesBasics/src/app/convert-length.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import pqm from "pqm";
 
 @Pipe({
   name: 'convertLength'
 })
 export class ConvertLengthPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, ...args: unknown[]): unknown {
+    let lengthInInches = parseInt(value);
+    if (!isNaN(lengthInInches)) {
+      return pqm.quantity(value,"in").in("cm");
+    } else {
+      return value;
+    }
   }
 
 }
