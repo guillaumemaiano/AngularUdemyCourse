38bf4b3fbf9b9d2d2dbd384ba020679ffe9ca794
Guillaume Maiano <guillaume@maiano.fr>
2020-08-30 19:40:34 +0200

    With an adaptable bar

--- AngularUdemyJobs/src/app/app.component.ts
+++ AngularUdemyJobs/src/app/app.component.ts
@@ -33,6 +33,97 @@ export class AppComponent {
       company: "BIG OIL SS",
       url:'https://images.unsplash.com/photo-1562237553-fd52cb2067b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
       date: ''
+    },
+    ////////
+    {
+      company: "BIG CORP SAS",
+      url:'https://images.unsplash.com/photo-1527259216948-b0c66d6fc31f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "COFFEE SHOP SLTD",
+      url:'https://images.unsplash.com/photo-1508766917616-d22f3f1eea14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "SMALL STARTUP CO",
+      url:'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80',
+      date: ''
+    },
+    {
+      company: "BIG OIL SS",
+      url:'https://images.unsplash.com/photo-1562237553-fd52cb2067b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "BIG CORP SAS",
+      url:'https://images.unsplash.com/photo-1527259216948-b0c66d6fc31f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "COFFEE SHOP SLTD",
+      url:'https://images.unsplash.com/photo-1508766917616-d22f3f1eea14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "SMALL STARTUP CO",
+      url:'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80',
+      date: ''
+    },
+    {
+      company: "BIG OIL SS",
+      url:'https://images.unsplash.com/photo-1562237553-fd52cb2067b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "BIG CORP SAS",
+      url:'https://images.unsplash.com/photo-1527259216948-b0c66d6fc31f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "COFFEE SHOP SLTD",
+      url:'https://images.unsplash.com/photo-1508766917616-d22f3f1eea14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "SMALL STARTUP CO",
+      url:'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80',
+      date: ''
+    },
+    {
+      company: "BIG OIL SS",
+      url:'https://images.unsplash.com/photo-1562237553-fd52cb2067b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "BIG CORP SAS",
+      url:'https://images.unsplash.com/photo-1527259216948-b0c66d6fc31f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "COFFEE SHOP SLTD",
+      url:'https://images.unsplash.com/photo-1508766917616-d22f3f1eea14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
+    },
+    {
+      company: "SMALL STARTUP CO",
+      url:'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80',
+      date: ''
+    },
+    {
+      company: "BIG OIL SS",
+      url:'https://images.unsplash.com/photo-1562237553-fd52cb2067b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80',
+      date: ''
     }
   ];
+
+
+  shouldDisplayPageItem(item: number): boolean {
+    if (item == 0) {
+      return true;
+    } else if (item < this.currentPage + 3 && item > this.currentPage - 3) {
+      return true;
+    }
+    return false;
+  };
 }
