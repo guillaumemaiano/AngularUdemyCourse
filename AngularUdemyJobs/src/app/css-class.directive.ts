1197a590fd45a084698b813fd7295e3d4c571361
Guillaume Maiano <guillaume@maiano.fr>
2020-08-31 11:28:28 +0200

    Property assignments in directives are more complex than they look

--- AngularUdemyJobs/src/app/css-class.directive.ts
+++ AngularUdemyJobs/src/app/css-class.directive.ts
@@ -1,12 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCssClass]'
 })
 export class CssClassDirective {
+  @Input() customColor: String;
 
   constructor(private element: ElementRef) { 
-    element.nativeElement.style.backgroundColor = 'darkGrey';
+    // this is not an actual solution, it demonstrates that the constructor runs BEFORE the template sets the property
+    setTimeout(() => {
+      element.nativeElement.style.backgroundColor = this.customColor || 'darkGrey';
+    }, 5000);
   }
-
 }
