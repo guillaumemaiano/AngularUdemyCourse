2f858d9e27b6213da3fcface63eacaa0d9e248da
Guillaume Maiano <guillaume@maiano.fr>
2020-09-03 16:19:42 +0200

    a simple segment exemple

--- AngularUdemyModules/src/app/elements/elements.module.ts
+++ AngularUdemyModules/src/app/elements/elements.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { SegmentComponent } from './segment/segment.component';
+import { SharedModule } from "../shared/shared.module";
 
 
 @NgModule({
-  declarations: [ElementsHomeComponent, PlaceholderComponent],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, SegmentComponent],
   imports: [
     CommonModule,
+    SharedModule,
     ElementsRoutingModule
   ],
   exports: [ElementsHomeComponent]
