a2c8ee36300885b9aa82ae22fa33c7b9d4a1658e
Guillaume Maiano <guillaume@maiano.fr>
2020-09-03 23:58:17 +0200

    Non reusable table

--- AngularUdemyModules/src/app/collections/collections-home/collections-home.component.ts
+++ AngularUdemyModules/src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    { name: "Kennedy", killed: true, succession: 35 },
+    { name: "Lincoln", killed: true, succession: 16 },
+    { name: "Trump", killed: false, succession: 45 }
+  ];
+  headers = [
+    { key: 'name', label: 'President' },
+    { key: 'succession', label: "Order of Succession" },
+    { key: 'killed', label: 'Assassinated while in Office' }
+  ];
   constructor() { }
 
   ngOnInit(): void {
